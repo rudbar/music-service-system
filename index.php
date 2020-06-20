5622aa6a69f452c696790f61c6404a6966628d09
Ian Kachan <kachan.yanf@gmail.com>
2020-06-20 23:45:14 +0500

    Added teh butthons to playbar section

--- index.php
+++ index.php
@@ -17,8 +17,89 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Велком</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    hello
+    
+
+	<div id="nowPlayingBarContainer">
+		
+		<div id="nowPlayingBar">
+			
+			<div id="nowPlayingLeft">
+				
+			</div>
+
+			<div id="nowPlayingCenter">
+				
+
+				<div class="content playerControls">
+					
+					<div class="buttons">
+
+						<button class="controlButton shuffle" title="Кнопка перемешать музыку">
+							<img src="assets/images/icons/shuffle.png" alt="Перемешать">
+						</button>
+
+						<button class="controlButton previous" title="Кнопка предидущая песня">
+							<img src="assets/images/icons/previous.png" alt="Предидущая">
+						</button>
+
+						<button class="controlButton play" title="Кнопка начать играть">
+							<img src="assets/images/icons/play.png" alt="Начать">
+						</button>
+
+						<button class="controlButton pause" title="Кнопка остановить музыку" style="display: none;">
+							<img src="assets/images/icons/pause.png" alt="Остановить">
+						</button>
+
+						<button class="controlButton next" title="Кнопка Следующая песня">
+							<img src="assets/images/icons/next.png" alt="Следующая">
+						</button>
+
+						<button class="controlButton repeat" title="Кнопка повторять песню">
+							<img src="assets/images/icons/repeat.png" alt="Повторять">
+						</button>
+						
+					</div>
+
+
+					<div class="playbackBar">
+						
+						<span class="progressTime current">0.00</span>
+
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+
+						<span class="progressTime remaining">0.00</span>
+
+
+					</div>
+
+
+
+				</div>
+
+
+			</div>
+
+			<div id="nowPlayingRight">
+				
+			</div>
+
+
+
+
+
+
+		</div>
+
+	</div>
+
+
 </body>
 </html>
