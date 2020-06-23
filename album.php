b7021e95544fad6dedf5ff30cdf2c01a03e66d53
Ian Kachan <kachan.yanf@gmail.com>
2020-06-23 23:08:17 +0500

    Created the album page and added teh content into there

--- album.php
+++ album.php
@@ -7,14 +7,24 @@ else {
 	header("Location: index.php");
 }
 
-$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-$album = mysqli_fetch_array($albumQuery);
-
-echo $album['title'];
-
+$album = new Album($con, $albumId);
+$artist = $album->getArtist();
 ?>
 
+<div class="entityInfo">
+	
+	<div class="leftSection">
+		<img src="<?php echo $album->getArtworkPath(); ?>">
+	</div>
 
+	<div class="rightSection">
+		<h2><?php echo $album->getTitle(); ?></h2>
+		<p><?php echo $artist->getName(); ?></p>
+		<p><?php echo $album->getNumberOfSongs(); ?> композиций</p>
+
+	</div>
+
+</div>
 
 
 
