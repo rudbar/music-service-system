6efc313226c949e53980fba3013dbbccde90a67c
Ian Kachan <kachan.yanf@gmail.com>
2020-06-25 23:10:42 +0500

    Added the audio object and included jQuery

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,9 @@ else {
     <title>Велком</title>
 
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 <body>
 
