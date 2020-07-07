9ecf5c652f9494c4bc3afd147557f9164d677114
Ian Kachan <kachan.yanf@gmail.com>
2020-07-07 22:05:28 +0500

    Added the user class and output the playlists

--- yourMusic.php
+++ yourMusic.php
@@ -10,6 +10,44 @@ include("includes/includedFiles.php");
 		<div class="buttonItems">
 			<button class="button green" onclick="createPlaylist()">НОВЫЙ ПЛЕЙЛИСТ</button>
 		</div>
+
+
+
+		<?php
+			$username = $userLoggedIn->getUsername();
+
+			$playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
+
+			if(mysqli_num_rows($playlistsQuery) == 0) {
+				echo "<span class='noResults'>У вас еще нет плейлистов.</span>";
+			}
+
+			while($row = mysqli_fetch_array($playlistsQuery)) {
+
+				echo "<div class='gridViewItem'>
+
+						<div class='playlistImage'>
+							<img src='assets/images/icons/playlist.png'>
+						</div>
+						
+						<div class='gridViewInfo'>"
+							. $row['name'] .
+						"</div>
+
+					</div>";
+
+
+
+			}
+		?>
+
+
+
+
+
+
+
+
 	
 	</div>
 
